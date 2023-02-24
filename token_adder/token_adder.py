9d13f8d2b7782de0ba04bcfb9833dbfb0834cc4c
Derrell Downey Jr <derrell.downey@gmail.com>
2023-02-24 10:42:32 -0500

    fixed script

--- token_adder/token_adder.py
+++ token_adder/token_adder.py
@@ -1,8 +1,5 @@
 """
 Simple script to format token list into switch statement for cs230 - Programming Languages lab 3
-
-Works well besides after each line read theres a \n which means that you need to manually delete each new line
-Not a big enough deal that I need to fix it right now but something to improve later if needed
 """
 
 # starting token const
@@ -13,10 +10,12 @@ file2 = open("/Users/derrelldowney/projects/code/scripts/token_adder/get_token_n
 
 line = file1.readline()
 while line != '':
-    token_name = line 
-    for token in token_name:
-        file2.write("case {}:\n\ttoken_name = \"{}\";\n\tbreak;\n".format(token_const, token))
-        token_const = token_const+1
+    
+    token_name = line.rstrip('\n') 
+    print(token_name)
+    
+    file2.write("case {}:\n\ttoken_name = \"{}\";\n\tbreak;\n".format(token_const, token_name))
+    token_const = token_const+1
     line = file1.readline()
 
 

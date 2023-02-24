0d8f3057714cea1e9d3bdb43e741b84466173a16
Derrell Downey Jr <derrell.downey@gmail.com>
2023-02-24 11:57:21 -0500

    fixed script

--- token_adder/token_adder.py
+++ token_adder/token_adder.py
@@ -5,15 +5,23 @@ Simple script to format token list into switch statement for cs230 - Programming
 # starting token const
 token_const = 258
 
-file1 = open("/Users/derrelldowney/projects/code/scripts/token_adder/tokens.txt", "r")
-file2 = open("/Users/derrelldowney/projects/code/scripts/token_adder/get_token_name_switch.txt", "w")
+# path for file 1
+file1_path = "/Users/derrelldowney/projects/code/scripts/token_adder/tokens.txt"
+# path fr file 2
+file2_path = "/Users/derrelldowney/projects/code/scripts/token_adder/get_token_name_switch.txt"
+
+file1 = open(file1_path, "r")
+file2 = open(file2_path, "w")
 
 line = file1.readline()
+
+# staying in the while loop until the EOF
 while line != '':
     
     token_name = line.rstrip('\n') 
-    print(token_name)
+    # print(token_name)
     
+    # writing the switch format to file2
     file2.write("case {}:\n\ttoken_name = \"{}\";\n\tbreak;\n".format(token_const, token_name))
     token_const = token_const+1
     line = file1.readline()
